5e38fd4659ebbaaf7c42d1405f04e4055a32afd9
PetNoire <noire@gmx.ca>
2020-04-09 16:36:25 -0600

    added employee/getEmployee

--- Main.cs
+++ Main.cs
@@ -54,7 +54,26 @@ namespace LightspeedNET
             this.lsAccount = ((lsAccounts)Deserializer.Deserialize(TextReader)).Account[0];
 
         }
+        public Employee GetEmployee(int EmployeeID)
+        {
+            var acc = AuthenticationClient.Account;
+            var request = new OAuth2RefreshRequest(AuthenticationClient.Authenticator, "GET",
+                                                new Uri($"https://cloud.lightspeedapp.com/API/Employee")
+                                                , null, ref acc);
+            AuthenticationClient.Account = acc;
+            var task = Task.Run(async () => await request.GetResponseAsync());
+            var response = task.Result;
 
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                throw new ExpiredTokenException();
+            }
+            var content = response.GetResponseText();
+            TextReader TextReader = new StringReader(content);
+            var Deserializer = new System.Xml.Serialization.XmlSerializer(typeof(Employee));
+            return ((Employee)Deserializer.Deserialize(TextReader));
+
+        }
 
         public void UpdateAccount(Account account)
         {
---
+++ Models/Common/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace LightspeedNET.Models.Common
+{
+    [XmlType("employee")]
+    public class Employee
+    {
+        [XmlElement("employeeID")]
+        public int EmployeeID { get; set; }
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+        [XmlIgnore]
+        public string FullName { get { return FirstName + " " + LastName; } }
+        [XmlElement("lockOut")]
+        public bool LockedOut { get; set; }
+        [XmlElement("archived")]
+        public bool Archived { get; set; }
+        [XmlElement("contactID")]
+        public int ContactID { get; set; }
+        [XmlElement("clockInEmployeeHoursID")]
+        public int ClockInEmployeeHoursID { get; set; }
+        [XmlElement("employeeRoleID")]
+        public int EmployeeRoleID { get; set; }
+        [XmlElement("limitToShopID")]
+        public int LimitToShopID { get; set; }
+        [XmlElement("lastShopID")]
+        public int LastShopID { get; set; }
+        [XmlElement("lastSaleID")]
+        public int LastSaleID { get; set; }
+        [XmlElement("lastRegisterID")]
+        public int LastRegisterID { get; set; }
+        [XmlElement("Contact")]
+        public Contact Contact { get; set; }
+        [XmlElement("EmployeeRole")]
+        public EmployeeRole EmployeeRole { get; set; }
+        [XmlElement("EmplyeeRights")]
+        public EmployeeRights EmployeeRights { get; set; }
+        [XmlElement("timeStamp")]
+        public DateTime TimeStamp { get; set; }
+
+    }
+
+    public class EmployeeRights
+    {
+        [XmlElement("employeeRightID")]
+        public int EmplyeeRightID { get; set; }
+        [XmlElement("name")]
+        public string Name { get; set; }
+    }
+
+    [XmlType("employeeRole")]
+    public class EmployeeRole
+    {
+        [XmlElement("employeeRoleID")]
+        public int EmployeeRoleID { get; set; }
+        [XmlElement("name")]
+        public string Name { get; set; }
+        [XmlElement("EmployeeRights")]
+        public EmployeeRights EmployeeRights { get; set; }
+    }
+}
